f700ef19623b240058f4bf36ec04a196f02a0020
Aditya Kamat <kamat.a@northeastern.edu>
2021-02-07 17:42:37 -0500

    added dotnet core project

---
+++ Pages/AllTheStars.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace webapp.Pages
+{
+    public class AllTheStarsModel : PageModel
+    {
+        private readonly ILogger<AllTheStarsModel> _logger;
+
+        public AllTheStarsModel(ILogger<AllTheStarsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/NotAfraid.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace webapp.Pages
+{
+    public class NotAfraidModel : PageModel
+    {
+        private readonly ILogger<NotAfraidModel> _logger;
+
+        public NotAfraidModel(ILogger<NotAfraidModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
